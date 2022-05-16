ac8bf9e2491f0d82e3b018cdc7a8d34cc725966e
Hassan Saber <hassansaberkarin@gmail.com>
2022-05-16 08:44:57 +0430

    done

--- js/details.js
+++ js/details.js
@@ -1,1 +1,27 @@
 // javascript for details.html
+const id = new URLSearchParams(window.location.search).get("id");
+const container = document.querySelector(".details");
+const deleteBtn = document.querySelector(".delete");
+
+const renderDetails = async () => {
+  const res = await fetch("http://localhost:3000/posts/" + id);
+  const post = await res.json();
+
+  const template = `
+  <h1>${post.title}</h1>
+  <P>${post.body}</p>
+  `;
+
+  container.innerHTML = template;
+};
+
+const deletePost = async (e) => {
+  await fetch("http://localhost:3000/posts/" + id, {
+    method: "DELETE",
+  });
+
+  window.location.replace("/index.html");
+};
+
+deleteBtn.addEventListener("click", deletePost);
+window.addEventListener("DOMContentLoaded", () => renderDetails());
--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
 // javascript for index.html
+const container = document.querySelector(".blogs");
+const searchForm = document.querySelector(".search");
+
+const renderPosts = async (term) => {
+  let uri = "http://localhost:3000/posts?_sort=likes&_order=desc";
+
+  if (term) {
+    uri += `&q=${term}`;
+  }
+
+  const res = await fetch(uri);
+  const posts = await res.json();
+
+  let template = "";
+  posts.forEach((post) => {
+    template += `
+    <div class="post">
+      <h2>${post.title}</h2>
+      <p><small>${post.likes} likes</small></p>
+      <p>${post.body.slice(0, 200)}</p> 
+      <a href="/details.html?id=${post.id}">read more...</a>
+    </div>
+    `;
+  });
+
+  container.innerHTML = template;
+};
+
+const searchFormSubmit = (e) => {
+  e.preventDefault();
+
+  renderPosts(searchForm.term.value.trim());
+};
+
+searchForm.addEventListener("submit", searchFormSubmit);
+window.addEventListener("DOMContentLoaded", () => renderPosts());
